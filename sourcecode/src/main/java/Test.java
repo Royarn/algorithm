177e8a1702f0f069c164d9b4818a97da23f79757
royarn <763094810@qq.com>
2019-03-08 10:46:46 +0800

    original

---
+++ sourcecode/src/main/java/Test.java
@@ -0,0 +1,10 @@
+package main.java;
+
+/**
+ * Description:
+ *
+ * @author lizhiqiang
+ * @date 2019-03-08
+ */
+public class Test {
+}
---
+++ src/com/royarn/original/Test.java
@@ -0,0 +1,14 @@
+package main.java;
+
+/**
+ * Description:
+ *
+ * @author lizhiqiang
+ * @date 2019-03-08
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
